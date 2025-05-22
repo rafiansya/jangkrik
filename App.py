0411225773bf38e589d1ab8bf3e8b3da0b827727
rafiansya <115703789+rafiansya@users.noreply.github.com>
2025-05-22 09:08:56 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="apakah musim depan tsunami trophy?")
 st.title("ONANA SUKA BANANA!")
 st.write("MAIN SG BENER SU")
 st.image ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTZCaFSeWstJqL-rzPMk7rnZKExPymU7lSiA&s", caption=" JAWA!", use_column_width=True)
