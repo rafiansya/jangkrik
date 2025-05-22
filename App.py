775d973030df0d2f8dfc3b324654439b6a77a446
rafiansya <115703789+rafiansya@users.noreply.github.com>
2025-05-22 08:49:11 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("STD!")
+st.title("ONANA SUKA BANANA!")
 st.write("MAIN SG BENER SU")
 st.image ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTZCaFSeWstJqL-rzPMk7rnZKExPymU7lSiA&s", caption=" JAWA!", use_column_width=True)
