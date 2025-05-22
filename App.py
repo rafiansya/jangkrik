47e8412573c5aead67d75c09d6c33f04a4d18348
rafiansya <115703789+rafiansya@users.noreply.github.com>
2025-05-22 08:46:59 +0700

    Create App.py

---
+++ App.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("STD!")
+st.write("MAIN SG BENER SU")
+st.image ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTZCaFSeWstJqL-rzPMk7rnZKExPymU7lSiA&s", caption=" JAWA!", use_column_width=True)
