5abe4270ee428536ae1c9700bbcbf2c7b88cce4a
rafiansya <115703789+rafiansya@users.noreply.github.com>
2025-05-22 09:03:22 +0700

    Update 2_tsunamitropy.py

--- pages/2_tsunamitropy.py
+++ pages/2_tsunamitropy.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+
+
+
+
+st.set_page_config(page_title="apakah musim depan tsunami trophy?")
+st.title("apakah musim depan tsunami trophy?")
+st.header("apakah musim depan tsunami trophy?")
